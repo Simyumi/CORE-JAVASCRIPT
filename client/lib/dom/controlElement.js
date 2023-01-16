56dc194facdba0311c743172f65167aa492d161b
kindtiger <seonbeom2@gmail.com>
2023-01-16 13:33:03 +0900

    [추가] visible,invisible 함수 만들기

--- client/lib/dom/controlElement.js
+++ client/lib/dom/controlElement.js
@@ -4,7 +4,11 @@ import { isElement } from "../utils/typeOf.js";
 
 
 
-
+/**
+ * @function isElement checkElement
+ * @param {HTMLElement} node
+ * @returns set disabled 
+ */
 
 export function disableElement(node){
 
@@ -15,12 +19,29 @@ export function disableElement(node){
 }
 
 
+
 export function enableElement(node){
   if(!isElement(node)){
     typeError('enableElement 함수의 인자는 DOM 요소 노드 이어야 합니다.')
   }
   
   node.disabled = false;
+}
+
+
+export function visibleElement(node){ // 보여야함
+  if(!isElement(node)){
+    typeError('visibleElement 함수의 인자는 DOM 요소 노드 이어야 합니다.')
+  }
+  node.hidden = false;
+}
+
+
+export function invisibleElement(node){ // 안보여야함 
+  if(!isElement(node)){
+    typeError('invisibleElement 함수의 인자는 DOM 요소 노드 이어야 합니다.')
+  }
+  node.hidden = true;
 }
 
 
