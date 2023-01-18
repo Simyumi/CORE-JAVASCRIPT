e173f7be6d9b9c71b6c1f6d1485e784d9f985a57
kindtiger <seonbeom2@gmail.com>
2023-01-18 16:02:15 +0900

    [추가] tiger 사용하기

--- client/main.js
+++ client/main.js
@@ -6,38 +6,29 @@ import {
   xhrData,
   insertLast,
   xhrPromise,
-  tiger
+  tiger,
+  delayP
  } from "./lib/index.js";
 
 
 
+ 
+
+ async function render(){
+
+   await delayP(2000);
+   let response = await tiger.get('https://jsonplaceholder.typicode.com/users/1')
 
 
 
-// xhrPromise
-// .get('https://jsonplaceholder.typicode.com/users/1')
-// .then((res)=>{
-//   insertLast(document.body,JSON.stringify(res));
-// })
-// .catch((err)=>{
-//   console.log(err);
-// })
+   console.log(response.data);
+ }
+
+
+ render()
 
 
 
-/* xhrData.get(
-  'https://jsonplaceholder.typicode.com/users/1',
-  (res)=>{
-    insertLast('body',JSON.stringify(res))
-  },
-  (err)=>{
-    insertLast('body','데이터 로딩에 실패했습니다.')
-  }
-)
-
- */
-
-/* 
 
 
- */
+
