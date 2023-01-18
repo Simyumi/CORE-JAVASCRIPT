a461d2115c2c2b2804ba38ab03106175abcd4426
kindtiger <seonbeom2@gmail.com>
2023-01-18 10:50:42 +0900

    [추가] 객체 합성

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -49,8 +49,28 @@ delayP()
 })
  */
 
-function delayP(shouldReject = false, timeout = 1000,data = '성공했습니다.', errorMessage = '알 수 없는 오류가 발생했습니다.'){
+const defaultOptions = {
+  shouldReject: false,
+  timeout: 1000,
+  data:'성공',
+  errorMessage: '알 수 없는 오류가 발생했습니다.'
+}
 
+function delayP(options = {}){
+
+  // defaultOptions
+
+  let config = {...defaultOptions}
+  
+  
+  // 객체 합성  mixin
+
+  config = {...config,...options};
+  
+  
+  const {shouldReject,data,errorMessage,timeout} = config;
+  
+  
   return new Promise((resolve, reject) => {
     
     setTimeout(() => {
@@ -60,7 +80,9 @@ function delayP(shouldReject = false, timeout = 1000,data = '성공했습니다.
 }
 
 
-delayP(false,1000,'진짜 성공','오류가 발생했다!!').then((res)=>{
+delayP({
+  data: '안녕',
+}).then((res)=>{
   console.log(res); // 진짜 성공
 })
 
